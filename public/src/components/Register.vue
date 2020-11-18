da896cf00ac3b0729845185bfb3cc0882ef90993
bobrov76 <50507472+bobrov76@users.noreply.github.com>
2020-11-18 11:36:05 +0700

    add bootstrap and localization

--- public/src/components/Register.vue
+++ public/src/components/Register.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="form">
-    <h1>Регистрация</h1>
-    <div id="role">
-      <input id="student" type="radio" value="student" v-model="role">
-      <label for="student">Ученик</label>
-      <input id="teacher" type="radio" value="teacher" v-model="role">
-      <label for="teacher">Учитель</label>
+    <div class="container-fluid text-center mh-100 w-100 mt-4">
+      <div class="card p-3">
+        <h1 class="mb-3">{{$t('auth.signUnTitle')}}</h1>
+        <div class="d-flex justify-content-center">
+          <div class="p-2">
+            <div class="form-check-inline">
+              <label class="form-check-label">
+                <input type="radio" class="form-check-input" value="student" v-model="role" name="optradio">{{$t('auth.student')}}
+              </label>
+            </div>
+          </div>
+          <div class="p-2">
+            <div class="form-check-inline">
+              <label class="form-check-label">
+                <input type="radio" class="form-check-input" value="teacher" v-model="role" name="optradio">{{$t('auth.teacher')}}
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="form-group mt-3">
+          <input v-model="email" class="form-control mw-100 mb-3" :placeholder="$t('auth.email')" type="text">
+        </div>
+        <div class="form-group">
+          <input v-model="firstname" class="form-control mw-100 mb-3" :placeholder="$t('auth.firstname')" type="text">
+        </div>
+        <div class="form-group">
+          <input v-model="lastname" class="form-control mw-100 mb-3" :placeholder="$t('auth.lastname')" type="text">
+        </div>
+        <div class="form-group">
+          <input v-model="password" class="form-control mw-100 mb-3" :placeholder="$t('auth.password')" type="password">
+        </div>
+        <div class="form-group">
+          <button class="btn btn-outline-primary mw-75 w-75 m-auto mb-0" type="button" @click="register">{{$t('auth.signUpBtn')}}</button>
+        </div>
+      </div>
     </div>
-    <input v-model="email" type="text" placeholder="Электронная почта">
-    <input v-model="firstname" type="text" placeholder="Имя">
-    <input v-model="lastname" type="text" placeholder="Фамилия">
-    <input v-model="password" type="password" placeholder="Пароль">
-    <button @click="register">Зарегистрироваться</button>
   </div>
 </template>
 
@@ -42,9 +66,9 @@ export default {
   methods: {
     register() {
       this.$store
-      .dispatch('auth/register', {
-          data: this.registerData,
-      }).then((res) => {
+          .dispatch('auth/register', {
+            data: this.registerData,
+          }).then((res) => {
         this.$initSocket();
         this.$router.push('/');
       });
@@ -52,3 +76,16 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.card {
+  margin: 30px auto auto;
+  max-width: 400px;
+  min-width: 300px;
+  box-shadow:3px 3px 5px 0px rgba(50, 50, 50, 0.1);
+}
+@media only screen and (max-width: 768px) {
+  .container-fluid{
+    padding: 0!important;
+  }
+}
+</style>
